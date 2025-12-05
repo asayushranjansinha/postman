3193df89abed4ba858f416aee978e47f42c1279e
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2025-12-05 10:53:11 +0530

    feat: Setup prisma and migrate dev with test model

---
+++ src/lib/database.ts
@@ -0,0 +1,16 @@
+
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+const prisma = globalThis.prisma || new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'], 
+})
+
+if (process.env.NODE_ENV === 'development') {
+  globalThis.prisma = prisma
+}
+
+export default prisma
