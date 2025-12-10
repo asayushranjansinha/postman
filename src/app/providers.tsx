e717c80ef4963aa16b7df5f11895d9bbbb90d590
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2025-12-10 15:00:33 +0530

    chore: setup tanstack query with custom query client function

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,8 +3,38 @@ import React from "react";
 
 import { ThemeProvider } from "@/components/theme/ThemeProvider";
 import { Toaster } from "@/components/ui/sonner";
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserClient: QueryClient | undefined = undefined;
+
+export function getBrowserClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserClient) {
+      browserClient = makeQueryClient();
+    }
+    return browserClient;
+  }
+}
 
 export const RootProviders = ({ children }: { children: React.ReactNode }) => {
+  const client = getBrowserClient();
+
   return (
     <ThemeProvider
       attribute="class"
@@ -12,7 +42,7 @@ export const RootProviders = ({ children }: { children: React.ReactNode }) => {
       enableSystem
       disableTransitionOnChange
     >
-      {children}
+      <QueryClientProvider client={client}>{children}</QueryClientProvider>
       <Toaster closeButton position="top-center" duration={2000} />
     </ThemeProvider>
   );
